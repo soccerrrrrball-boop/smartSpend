f2a05ad3fd19da27db90e99b95ff6310ddf4b4be
chitreshkumar210-coder <YOUR_GITHUB_EMAIL>
2025-11-18 21:07:03 +0530

    describe your changes

--- backend/src/main/java/com/fullStack/expenseTracker/controllers/TransactionTypeController.java
+++ backend/src/main/java/com/fullStack/expenseTracker/controllers/TransactionTypeController.java
@@ -1,15 +1,15 @@
 package com.fullStack.expenseTracker.controllers;
 
-import com.fullStack.expenseTracker.services.TransactionTypeService;
-import com.fullStack.expenseTracker.models.TransactionType;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.fullStack.expenseTracker.models.TransactionType;
+import com.fullStack.expenseTracker.services.TransactionTypeService;
 
 @RestController
 @RequestMapping("/mypockit/transactiontype")
